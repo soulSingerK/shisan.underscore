80fd55592e5e7470a354699f3c1296f330751e58
Gshisan <425139963@qq.com>
2018-12-26 11:56:52 +0800

    feat：throttle (节流函数 )

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ let count = 1
 const container = document.getElementById('container')
 function getUserAction() {
   container.innerHTML = count++
-  return 1
 }
-const setUserAction = _.debounce(getUserAction, 100)
-container.onmousemove = setUserAction
-container.onclick = function() {
-  setUserAction.cancel()
-}
+
+
+// const setUserAction = _.debounce(getUserAction, 100)
+// container.onmousemove = setUserAction
+// container.onclick = function() {
+//   setUserAction.cancel()
+// }
 // container.onmousemove = getUserAction
+
+container.onmousemove = _.throttle(getUserAction, 1000, { trailing: true, leading: false })
--- src/underscore.shisan.js
+++ src/underscore.shisan.js
@@ -32,4 +32,49 @@ underscore.prototype.debounce = function(func, wait, immediate) {
   }
   return debounced
 }
+
+/**
+ * 
+ * @param {*} func 
+ * @param {*} wait 
+ * @param {Object} options
+ * options两个设置
+ * leading 表示事件触发时回调是否立即执行回调一次 
+ * trailing 表示事件结束时是否再调用一次回调
+ */
+underscore.prototype.throttle = function(func, wait, options) {
+  let timeout, result
+  let previous = 0
+  options = options || {}
+  const later = (...args) => {
+    previous = options.leading === false ? 0 : Date.now()
+    func.apply(this, args)
+    if (!timeout) args = null
+  }
+
+  const throttled = (...args) => {
+    const now = Date.now()
+    if (!previous && options.leading === false) previous = now
+    const remaining = wait - ( now - previous )
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = now
+      result = func.apply(this, args)
+      if (!timeout) args = null
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining)
+    }
+  }
+  throttled.cancel = function() {
+    clearTimeout(timeout)
+    previous = 0
+    timeout = null
+  }
+  return throttled
+}
+
+
 export default underscore
